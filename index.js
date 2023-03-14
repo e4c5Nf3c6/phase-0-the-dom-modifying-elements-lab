5f5b717d078cfe45cadddb35747e520b5903edb7
Maria Jenkins <macjenkin@gmail.com>
2023-03-14 02:29:06 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.removeAttribute('id');
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+newHeader.textContent = "Maria is the champion";
+
+document.body.append(newHeader);
